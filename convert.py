ddd031e6827baa00e7ad1d751ab28e77c82024ef
Javier M. Duarte <jduarte@ucsd.edu>
2021-09-28 20:55:16 -0700

    update

--- convert.py
+++ convert.py
@@ -4,7 +4,7 @@ import os
 import setGPU
 # edit depending on where Vivado is installed:
 # os.environ['PATH'] = '/<Xilinx installation directory>/Vivado/<version>/bin:' + os.environ['PATH']
-os.environ['PATH'] = '/xilinx/Vivado/2019.1/bin:' + os.environ['PATH']
+os.environ['PATH'] = '/xilinx/Vivado/2019.2/bin:' + os.environ['PATH']
 import tensorflow as tf
 from qkeras.utils import _add_supported_quantized_objects
 import hls4ml
@@ -48,48 +48,45 @@ for layer in config['LayerName'].keys():
     config['LayerName'][layer]['Trace'] = False
     if 'input' in layer:
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<32,16>'
-    if 'batchnorm' in layer:
+    elif 'batchnorm' in layer:
         config['LayerName'][layer]['accum_t'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['scale'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['bias'] = 'ap_fixed<32,16>'
-    if 'linear' in layer:
+    elif 'linear' in layer:
         config['LayerName'][layer]['Precision'] = {}
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<9,6>'
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<9,6>'
-    if 'tmul' in layer and 'linear' not in layer:
+    elif 'tmul' in layer and 'linear' not in layer:
         config['LayerName'][layer]['Precision']['weight'] = 'ap_uint<1>'
         config['LayerName'][layer]['Precision']['bias'] = 'ap_uint<1>'
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<9,6>'
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<9,6>'
-    if 'q_activation' in layer:
+    elif 'q_activation' in layer:
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<9,6>'
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<8,3,AP_RND,AP_SAT>'
-    if 'q_conv1d' in layer:
+    elif 'q_conv1d' in layer:
         config['LayerName'][layer]['accum_t'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<9,6>'
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<9,6>'
-    if 'q_dense' in layer:
+    elif 'q_dense' in layer:
         config['LayerName'][layer]['accum_t'] = 'ap_fixed<32,16>'
         config['LayerName'][layer]['Precision']['result'] = 'ap_fixed<9,6>'
         config['LayerName'][layer]['Precision']['default'] = 'ap_fixed<9,6>'
 
-cfg = hls4ml.converters.create_config(part='xc7z020clg400-1')
-cfg['HLSConfig'] = config
-cfg['IOType'] = 'io_stream'
-cfg['Backend'] = 'Vivado'
-cfg['ClockPeriod'] = 5
-cfg['KerasModel'] = model
-cfg['OutputDir'] = 'hls_output'
-
 print("-----------------------------------")
-print(cfg)
+print(config)
 print("-----------------------------------")
 
-hls_model = hls4ml.converters.keras_to_hls(cfg)
+hls_model = hls4ml.converters.convert_from_keras_model(model,
+                                                       hls_config=config,
+                                                       io_type='io_stream',
+                                                       output_dir='hls_output',
+                                                       part='xcvu9p-flgb2104-2-i')
+hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='hls4ml_in_plot.png')
 hls_model.compile()
-                                                                                    
+
 X_test = np.random.rand(1, 8, 3)
 y_keras = model.predict(X_test)
 y_hls = hls_model.predict(X_test).reshape(y_keras.shape)
@@ -98,4 +95,4 @@ print('hls4ml:\n', y_hls)
 # Synthesize                                                                                                                           
 hls_model.build(csim=False)
 # Reports                                                                                                                              
-hls4ml.report.read_vivado_report(output_dir)
+hls4ml.report.read_vivado_report('hls_output')
