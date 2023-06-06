e05db1b0df0834d3fb396400ba8b85e6c5fe85b7
Zhiqiang Que <quezhiqiang@gmail.com>
2023-06-06 22:23:32 +0100

    deal with non-existing acc.txt file

--- train/list.py
+++ train/list.py
@@ -9,8 +9,9 @@ b = []
 
 for mname in list_dir:
     if (mname.split("_")[0] == 'nconst32'):
-        a = np.loadtxt('{}/acc.txt'.format(mname))
-        b.append([a[0], mname.split("_")] )
+        if (os.path.isfile('{}/acc.txt'.format(mname))):
+            a = np.loadtxt('{}/acc.txt'.format(mname))
+            b.append([a[0], mname.split("_")] )
 
 
 b_sorted = sorted ([b[i] for i in range(len(b))], reverse=True)
